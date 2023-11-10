191501e8102d236f4df792827d9bacaaa5fca87d
saif-uddin-shrabon <saif06065@gmail.com>
2023-11-11 00:45:39 +0600

    Add Splash Screen & Production version-1.0

---
+++ app/src/main/java/com/algostack/leaderboard/view/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.algostack.leaderboard.view
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.algostack.leaderboard.R
+import com.algostack.leaderboard.databinding.FragmentSplashScreenBinding
+
+@Suppress
+class SplashScreen : Fragment() {
+
+    private var _binding : FragmentSplashScreenBinding ?= null
+    private val binding get() = _binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       _binding = FragmentSplashScreenBinding.inflate(inflater,container,false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_leaderboardHome)
+        }, 3000)
+
+        return binding?.root
+    }
+
+}
